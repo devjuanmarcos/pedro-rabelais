0060eece1b1a0e99ad64843968dca0964da4f7ee
Juan Marcos <jncontatojn@gmail.com>
2025-12-10 11:22:58 -0300

    fix: update params type to Promise for async handling in RootLayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   const resolvedParams = await params;
   const messages = await getMessages(resolvedParams.locale as any);
